b0e045ee7fa84d19ca7f5851b049a0d2b619882a
Mike Solomon <mike.solomon@coinbase.com>
2025-06-01 19:47:36 -0400

    rpc

--- web/src/constants/chain.ts
+++ web/src/constants/chain.ts
@@ -1,3 +1,7 @@
+import { defineChain } from "thirdweb";
 import { base } from "thirdweb/chains";
 
-export const chain = base;
+export const chain = defineChain({
+  ...base,
+  rpc: `https://api.developer.coinbase.com/rpc/v1/base/${process.env.NEXT_PUBLIC_CDP_API_KEY}`,
+});
